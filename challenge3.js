ea4254427a1e0e673395798478712fd79135ad61
Molagare Modiba <molagare@gmail.com>
2023-06-22 14:14:11 +0200

    first commit. completed iwa2

---
+++ challenge3.js
@@ -0,0 +1,13 @@
+/* It is important to show the following message in order
+*To assure users
+*To scare hackers
+*To impress investors
+*/
+
+console.warn('security scan starting')
+
+const warning = "wait for the scan to finish"
+
+console.log(warning)
+
+/* Wait for the scan to complete before closing the browser */
